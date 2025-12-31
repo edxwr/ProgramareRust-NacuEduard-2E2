3b70b1c7f09200a0d9b6fd37124401b388cc59c1
edward <edyfry89@gmail.com>
2025-12-31 02:38:16 +0200

    Comanda de quote 	new file:   k9.db 	modified:   src/main.rs

--- proiect_k9/src/main.rs
+++ proiect_k9/src/main.rs
@@ -1,10 +1,19 @@
 use poise::serenity_prelude as serenity;
 use dotenvy::dotenv;
 use std::env;
+use sqlx::SqlitePool;
 
-struct Data {}
+struct Data {
+    database : SqlitePool
+}
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
+
+#[derive(sqlx::FromRow)]
+struct Quote{
+    author : String,
+    text : String
+}
 
 #[poise::command(slash_command)]
 async fn ping(ctx: Context<'_>) -> Result<(), Error>{
@@ -12,20 +21,32 @@ async fn ping(ctx: Context<'_>) -> Result<(), Error>{
     Ok(())
 }
 
+#[poise::command(slash_command)]
+async fn quote(ctx: Context<'_>) -> Result<(), Error>{
+    let result : Quote = sqlx::query_as(
+        "SELECT author, text FROM quotes ORDER BY RANDOM() LIMIT 1"
+    ).fetch_one(&ctx.data().database).await?;
+
+    ctx.say(format!("> {}\n— *{}*", result.text, result.author)).await?;
+    Ok(())
+}
+
 #[tokio::main]
 async fn main(){
     dotenv().ok();
     let token = env::var("DISCORD_TOKEN").expect("NO DISCORD TOKEN");
+    let database_url = env::var("DATABASE_URL").expect("NO DATABASE URL");
+    let db_pool = SqlitePool::connect(&database_url).await.expect("Error when connecting to the database");
 
     let framework = poise::Framework::builder()
         .options(poise::FrameworkOptions {
-            commands: vec![ping()],
+            commands: vec![ping(), quote()],
             ..Default::default()
         })
         .setup(|ctx, _ready, framework| {
             Box::pin(async move {
                 poise::builtins::register_globally(ctx, &framework.options().commands).await?;
-                Ok(Data{})
+                Ok(Data{ database : db_pool })
             })
         })
         .build();
